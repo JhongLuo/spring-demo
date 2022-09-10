323bd2b4062131cf8b16265e11b100824cd7e2f5
JhongLuo <1999luozhonghao@gmail.com>
2022-09-10 17:30:25 -0400

    Initial commit

---
+++ src/jhong/springdemo/MyApp.java
@@ -0,0 +1,10 @@
+package jhong.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		BaseballCoach theCoath = new BaseballCoach();
+		System.out.println(theCoath.getDailyWorkout());
+	}
+
+}
