69aac62752a291c8ff33bdcf19f36a32c446a7f8
JhongLuo <1999luozhonghao@gmail.com>
2022-09-15 23:04:25 -0400

    Spring MVC request mapping and form tags

---
+++ spring-mvc-demo/src/jhong/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,38 @@
+package jhong.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloWorldController {
+	
+	// need a controller method to show the initial form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// need a controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		String theName = request.getParameter("studentName");
+		model.addAttribute("message", "Yo! " + theName.toUpperCase());
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+		model.addAttribute("message", "Yohoo! " + theName.toUpperCase());
+		return "helloworld";
+	}
+}
---
+++ spring-mvc-demo/src/jhong/springdemo/mvc/Student.java
@@ -0,0 +1,26 @@
+package jhong.springdemo.mvc;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
